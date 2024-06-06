62ba813c209ad4bdb77e6ec0556da15b4d5a7924
javierCh77 <javierchavarria21@gmail.com>
2024-06-05 22:11:39 -0300

    update

--- src/app/dashboard/badge/page.tsx
+++ src/app/dashboard/badge/page.tsx
@@ -3,8 +3,12 @@ import { Badge } from "@/components/ui/badge"
 
 export default function Page() {
   return (
-    <div className="grid grid-cols-4 gap-2">
+    <div className="grid grid-cols-4 gap-4">
       <Badge>Badge</Badge>
+      <Badge variant='destructive'>destructive</Badge>
+      <Badge variant='secondary'>secondary</Badge>
+      <Badge variant='info'>info</Badge>
+      <Badge variant='success'>success</Badge>
     </div>
   );
 }
--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  "capitalize inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
   {
     variants: {
       variant: {
@@ -15,6 +15,11 @@ const badgeVariants = cva(
         destructive:
           "border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80",
         outline: "text-foreground",
+        info:
+        "border-transparent bg-blue-400 text-white shadow hover:bg-blue-500",
+        success:
+        "border-transparent bg-emerald-400 text-white shadow hover:bg-emerald-500",
+        
       },
     },
     defaultVariants: {
